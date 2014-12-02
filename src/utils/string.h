7f588d44fd455edab1134cc61ac4ef4c56f88bf7
Superjom <yanchunwei@outlook.com>
2014-12-02 21:03:09 +0800

    add string.h

---
+++ src/utils/string.h
@@ -0,0 +1,12 @@
+//
+//  string.h
+//  SwiftSnails
+//
+//  Created by Chunwei on 12/2/14.
+//  Copyright (c) 2014 Chunwei. All rights reserved.
+//
+//
+#ifndef SwiftSnails_utils_string_h_
+#define SwiftSnails_utils_string_h_
+
+#endif 
