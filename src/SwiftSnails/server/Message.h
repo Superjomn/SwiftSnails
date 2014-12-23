2d16cb5a399443c28c17f32dd33d13c9fdd2799d
Superjom <superjom@gmail.com>
2014-12-23 14:57:31 +0800

    move MessageMeta from BasicServer.h to Message.h

---
+++ src/SwiftSnails/server/Message.h
@@ -0,0 +1,60 @@
+namespace swift_snails {
+
+struct MessageMeta {
+    uint64_t message_class; // server-side function
+    uint64_t message_id;    // used for client's response
+    int client_id;          // to send response
+};
+
+class Message {
+public:
+    Message() {
+        PCHECK(0 == zmq_msg_init(&_zmg));
+    }
+    Message(const Message&) = delete;
+
+    Message(char& buf, size_t size) {
+        PCHECK(0 == zmq_msg_init_size(&_zmg, size));
+        memcpy(buffer(), buf, size);
+    }
+
+    ~Message() {
+        PCHECK(0 == zmq_msg_close(&_zmg));
+    }
+
+    Message& operator=(const Message&) = delete;
+    Message& operator=(Message&& other) {
+        if(this != &other) {
+            PCHECK(0 == zmq_msg_move(&_zmg, &other._zmg));
+        }
+        return *this;
+    }
+
+    size_t length() {
+        return zmq_msg_size(&_zmsg);
+    }
+
+    void reset() {
+        PCHECK(0 == zmq_msg_close(&_zmg));
+        PCHECK(0 == zmq_msg_init(&_zmg));
+    }
+
+    void resize(size_t newsize) {
+        PCHECK(0 == zmq_msg_close(&_zmg));
+        PCHECK(0 == zmq_msg_init_size(&_zmg, newsize));
+    }
+
+    void assign(char* buf, size_t size) {
+        resize(size);
+        memcpy(buffer(), buf, size);
+    }
+
+    char* buffer() {
+        return (char*)zmq_msg_data(&_zmg);
+    }
+
+private:
+    zmq_msg_t _zmg;
+};  // end class Message
+
+};  // end namespace swift_snails
