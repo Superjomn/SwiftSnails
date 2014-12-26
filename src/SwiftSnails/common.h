933995aef5869a2b26df5f1c2fdf06fac7b03d39
Superjom <superjom@gmail.com>
2014-12-26 13:59:40 +0800

    fix bugs in SwiftSnails/common.h

--- src/SwiftSnails/common.h
+++ src/SwiftSnails/common.h
@@ -8,13 +8,15 @@
 
 #ifndef SwiftSnails_SwiftSnails_common_h_
 #define SwiftSnails_SwiftSnails_common_h_
+#include "../utils/common.h"
+#include "../utils/string.h"
 namespace swift_snails {
 
 inline void zmq_bind_random_port(const std::string& ip, void* socket, std::string& addr, int& port) {
     const int nturns2try = 10000;
     for(int i = 0; i < nturns2try; i++) {
         port = 1024 + rand() % (65536 - 1024);
-        format_string(addr, "tcp://%s:%d", ip, port);
+        format_string(addr, "tcp://%s:%d", ip.c_str(), port);
         int res;
         PCHECK((res = zmq_bind(socket, addr.c_str()), 
                 res == 0 || errno == EADDRINUSE));  // port is already in use
@@ -44,8 +46,19 @@ public:
     explicit thread_guard(std::thread& t) :
         _t(t)
     { }
+    explicit thread_guard(std::thread&& t) : 
+        _t(t)
+    { }
+    explicit thread_guard(thread_guard&& other) :
+        _t(other._t)
+    { }
     thread_guard(thread_guard const&) = delete;
     thread_guard& operator=(thread_guard const&) = delete;
+
+    void join() {
+        CHECK(_t.joinable());
+        _t.join();
+    }
     ~thread_guard() {
         if(_t.joinable()) _t.join();
     }
