eccd0191cc45d5d1ee4038dc193318ebc9c9acc1
Superjom <superjom@gmail.com>
2014-12-27 10:11:24 +0800

    add doc to CallBackFunc

--- src/SwiftSnails/server/MasterServer.h
+++ src/SwiftSnails/server/MasterServer.h
@@ -13,28 +13,35 @@
 
 namespace swift_snails {
 
-/*
- * basic function:
- * receive Message and push message into a queue
- * messages in the queue will be processed by a thread-pool
- * the result will be sent to sender
- */
-
 struct MetaMessage : public MessageMetaBase {
     index_t message_id;     // used for client's response
     index_t client_id;      // to send response
     index_t shard_id;
 };
 
+/*
+ * meta_msg: Meta Message (binary type)
+ *          contain some meta infomation
+ * cont_msg: Content Message (text type, control message) , 
+ *          contain the content(such as ip list), the control message 
+ *          will be text
+ * obb: output text buffer(the response)
+ */
 typedef std::function<void(BinaryBuffer& meta_msg, TextBuffer& cont_msg, TextBuffer& obb)> MetaCallBack;
 
-class BasicMetaServer : public FunctionBasicServer<MetaMessage, char, MetaCallBack> {
+/*
+ * basic function:
+ * receive Message and push message into a queue
+ * messages in the queue will be processed by a thread-pool
+ * the result will be sent to sender
+ */
+class BasicMasterServer : public FunctionBasicServer<MetaMessage, char, MetaCallBack> {
 public:
     /*
      * local_addr: ip address of current node (meta server node)
      * // both metanode's ip and port should be specified manually
      */
-    BasicMetaServer(const std::string &local_addr, int port) {
+    BasicMasterServer(const std::string &local_addr, int port) {
         listen(local_addr);
     }
 private:
