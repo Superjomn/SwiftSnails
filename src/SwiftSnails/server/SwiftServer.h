c23e9f906b030712c40cf5db05ccb779c2889aee
Superjom <superjom@gmail.com>
2014-12-27 10:20:59 +0800

    add SwiftServer.h

---
+++ src/SwiftSnails/server/SwiftServer.h
@@ -0,0 +1,17 @@
+//
+//  SwiftServer.h
+//  SwiftSnails
+//
+//  Created by Chunwei on 12/23/14.
+//  Copyright (c) 2014 Chunwei. All rights reserved.
+//
+
+#ifndef SwiftSnails_SwiftSnails_server_SwiftServer_h_
+#define SwiftSnails_SwiftSnails_server_SwiftServer_h_
+
+namespace swift_snails {
+
+}; // end namespace swift_snails
+
+#endif
+
