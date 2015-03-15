855e22af662d0ec499c554a776e868e91840e882
Superjom <superjom@gmail.com>
2015-03-15 10:30:37 +0800

    add worker/init.h

---
+++ src/core/transfer_framework/worker/init.h
@@ -0,0 +1,15 @@
+//
+//  init.h
+//  SwiftSnails
+//
+//  Created by Chunwei on 3/15/15.
+//  Copyright (c) 2015 Chunwei. All rights reserved.
+//
+#ifndef SwiftSnails_core_transfer_framework_worker_init_h_
+#define SwiftSnails_core_transfer_framework_worker_init_h_
+
+namespace swift_snails {
+
+
+}; // end namespace swift_snails
+#endif
