043ddc516728d0e637992566810b01610812de45
Superjom <superjom@gmail.com>
2015-03-15 15:30:44 +0800

    init init.h

--- src/core/transfer_framework/worker/init.h
+++ src/core/transfer_framework/worker/init.h
@@ -7,8 +7,17 @@
 //
 #ifndef SwiftSnails_core_transfer_framework_worker_init_h_
 #define SwiftSnails_core_transfer_framework_worker_init_h_
-
+#include "../../transfer/Transfer.h"
 namespace swift_snails {
+
+/*
+ * init management of Worker Node
+ *
+ * register message_classes  and response_handlers
+ */
+class WorkerTransferInit {
+
+};  // end class WorkerTransferInit
 
 
 }; // end namespace swift_snails
