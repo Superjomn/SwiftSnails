7fd743cc91bc0a2190cee23b13cad1b34b848da3
Superjom <superjom@gmail.com>
2015-03-15 15:03:54 +0800

    add message_classes.h to register message_class ids

---
+++ src/core/transfer_framework/message_classes.h
@@ -0,0 +1,21 @@
+//
+//  message_classes.h
+//  SwiftSnails
+//
+//  Created by Chunwei on 3/15/15.
+//  Copyright (c) 2015 Chunwei. All rights reserved.
+//
+#ifndef SwiftSnails_core_transfer_framework_message_classes_h_
+#define SwiftSnails_core_transfer_framework_message_classes_h_
+namespace swift_snails {
+
+// registed message class ids
+enum MSG_CLS {
+
+
+
+};  // end enum MSG_CLS
+
+
+}; // end namespace swift_snails
+#endif
