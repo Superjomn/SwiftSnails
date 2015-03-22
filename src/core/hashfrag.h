f0f4731170b2226d0c5b0c81109ad72f4aa14bbf
Superjom <superjom@gmail.com>
2015-03-22 19:07:01 +0800

    init hashfrag.h

---
+++ src/core/hashfrag.h
@@ -0,0 +1,107 @@
+#pragma once
+#include "../../utils/all.h"
+namespace swift_snails {
+
+// get hash code
+template<typename Key>
+int hash_fn(const Key& key);
+
+uint64_t hash_fn<uint64_t>(uint64_t x) {
+    return get_hash_code(x);
+}
+
+/*
+ * Basic Hash Fragment
+ * without Replication, Fault Tolerance and Repair
+ */
+template<typename Key>
+class BasicHashFrag : public VirtualObject {
+public:
+    typedef typename Key key_t;
+    // num_nodes: initial number of nodes
+    explicit HashFrag() 
+    { 
+        std::call_once( _config_register_flag,
+            [] {
+                global_config().register_config("frag_num");
+            });
+    }
+    /*
+     * number of nodes should be setted by master
+     */
+    void set_num_nodes(int x) {
+        CHECK(x > 0);
+        _num_nodes = x;
+    }
+    // init nodes in the hash ring
+    // can be called only once
+    // and should be called by *Master Server*
+    // the worker nodes should not call it
+    void init() {
+        CHECK(num_nodes() > 0);
+        //num_nodes = global_config().register_config("init_node_num").to_int32();
+        frag_num = global_config().register_config("frag_num").to_int32();
+        _map_table.reset(new index_t[num_frags()]);
+        // divide the fragments
+        int num_frag_each_node = int(frag_num / num_nodes);
+        for(int i = 0; i < frag_num; i ++) {
+            _map_table[i] = int(i / num_frag_each_node);
+        }
+    }
+
+    int to_node_id(const key_t &key) {
+        CHECK(_map_table) << "map_table has not been inited";
+        int frag_id = hash_fn(key) % frag_num;
+        int node_id = _map_table[frag_id];
+        return node_id;
+    }
+
+    void serialize(BinaryBuffer &bb) const {
+        bb << num_nodes();
+        bb << num_frags();
+
+        for(int i = 0; i < num_frags(); i ++) {
+            bb << _map_table[i];
+        }
+    }
+
+    void deserialize(BinaryBuffer &bb) {
+        int num_nodes_, num_frags_;
+        bb >> num_nodes_;    
+        bb >> num_frags_;
+        CHECK( 
+            (_map_table && (num_frags_ == num_frags())) ||
+            (!_map_table)
+        );
+        _num_nodes = num_nodes_;
+        _num_frags = num_frags_;
+        // memory should not been inited
+        // the size of the map table will not be changed
+        for(int i = 0; i < num_frags(); i ++) {
+            bb << _map_table[i];
+        }
+    }
+
+    int num_nodes() const {
+        return _num_nodes;
+    }
+    int num_frags() const {
+        return _num_frags;
+    }
+
+
+private:
+    int _num_nodes = 0;
+    int _num_frags = 0;
+    // record visit frequency
+    //std::map<int, NodeVisitFreq> visit_freqs;
+    // register config 
+    static std::once_flag _config_register_flag;
+    std::unique_ptr _map_table;
+
+};  // class HashFrag
+static std::once_flag BasicHashFrag::once_flag;
+
+
+
+};  // end namespace swift_snails
