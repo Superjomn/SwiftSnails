f628269a321420054dcbde690999104ddf238eff
Superjom <superjom@gmail.com>
2015-03-23 11:16:06 +0800

    fix string.h split() bug

--- src/utils/string.h
+++ src/utils/string.h
@@ -31,15 +31,15 @@ std::string& trim(std::string &ss) {
 std::vector<std::string> split(const std::string &s, const std::string &delim) {
     std::vector<std::string> cols;
     size_t last = 0;
-    size_t index = s.find_first_of(delim, last);
-    while (index != std::string::npos) {
-        cols.push_back(s.substr(last, index - last));
-        last = index + 1;
-        index = s.find_first_of(delim, last);
-    }
-    if (index - last > 0) 
-    {
-        cols.push_back(s.substr(last, index - last)); 
+    size_t start = s.find_first_not_of(delim, last);
+    while (start != std::string::npos) {
+        last = s.find_first_of(delim, start);
+        if(last == std::string::npos) {
+            cols.push_back(s.substr(start));
+        } else {
+            cols.push_back(s.substr(start, last - start));
+        }
+        start = s.find_first_not_of(delim, last);
     }
     return std::move(cols);
 }
