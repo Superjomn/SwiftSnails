b785e3626464505d950d6ca4e6447d766ab2dff5
Superjom <superjom@gmail.com>
2015-03-23 20:06:23 +0800

    add sparse_access_method.h

--- src/core/parameter/sparse_access_method.h
+++ src/core/parameter/sparse_access_method.h
@@ -5,18 +5,18 @@
 //  Created by Chunwei on 3/21/15.
 //  Copyright (c) 2015 Chunwei. All rights reserved.
 //
-namespace mio_table {
+namespace swift_snails {
 
 template<typename Table, typename PullVal, typename PullParam>
 class PullAccessMethod {
 public:
+    typedef Table      table_t;
     typedef typename Table::key_t   key_t;
     typedef typename Table::value_t value_t;
-    typedef typename Table      table_t;
     // client side 
-    typedef typename PullVal    pull_val_t;
+    typedef PullVal    pull_val_t;
     // server side
-    typedef typename PullParam    pull_param_t;
+    typedef PullParam    pull_param_t;
 
     virtual void init_param(const key_t& key, pull_param_t &param) = 0;
     virtual void get_pull_value(const key_t &key, const pull_param_t &param, pull_val_t &val) = 0;
@@ -29,11 +29,11 @@ public:
 template<typename Table, typename PushVal, typename PushParam>
 class PushAccessMethod {
 public:
+    typedef Table      table_t;
     typedef typename Table::key_t   key_t;
     typedef typename Table::value_t value_t;
-    typedef typename Table      table_t;
-    typedef typename PushVal    push_val_t;
-    typedef typename PushParam  push_param_t;
+    typedef PushVal    push_val_t;
+    typedef PushParam  push_param_t;
 
     virtual void merge_push_value(const key_t& key, push_val_t &push_val, const push_val_t &other_push_val) = 0;
 
---
+++ src/unitest/core/parameter/sparse_access_method_test.h
@@ -0,0 +1,57 @@
+#include <climits>
+#include "gtest/gtest.h"
+#include "../../../core/parameter/sparsetable.h"
+#include "../../../core/parameter/sparse_access_method.h"
+using namespace std;
+using namespace swift_snails;
+
+namespace sparse_table_test {
+
+// 参数格式
+struct Param {
+    int a;
+    float b;
+};
+
+typedef float Val;
+
+typedef SparseTable<index_t, Param> table_t;
+
+class PullMethod : public PullAccessMethod<table_t, Val, Param> {
+
+public:
+    virtual void init_param(const key_t& key, pull_param_t &param)
+    {
+        param.a = 0;
+        param.b = 0.5;
+    }
+
+    virtual void get_pull_value(const key_t &key, const pull_param_t &param, pull_val_t &val) {
+        val = param.a + param.b;
+    }
+
+    virtual void apply_pull_value(const key_t &key, pull_param_t &param, const pull_val_t& val) {
+        param.a += val;
+        param.b += val;
+    }
+
+};  // class PullMethod
+
+TEST(SparseTable, access_method) {
+    table_t table;
+    auto access = make_pull_access<table_t, PullMethod>(table);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+};  // end namespace sparse_table_test
+
