a178fe87e044203e6d43956014ec023910f92807
Superjom <superjom@gmail.com>
2015-03-25 21:05:40 +0800

    global_pull_access.h code clean

--- src/core/parameter/global_pull_access.h
+++ src/core/parameter/global_pull_access.h
@@ -4,87 +4,43 @@
 #include "hashfrag.h"
 #include "global_param.h"
 namespace swift_snails {
-
 /*
-
-// bundle of requests
-template<class Key, class Val>
-class RecvParcel : public VirtualObject {
-public:
-    typedef Key key_t;
-    typedef Val val_t; 
-
-    void sent(int node_id) {
-        //std::lock_guard lock(mut);
-        //_rsp_rcv_flags[id] = false;
-        _num_reqs ++;
-    }
-    // return true if all responses have been received
-    bool recv(int node_id) {
-        std::lock_guard lock(mut); 
-        //_rsp_rcv_flags[id] = true;
-        _num_reqs --;
-        return _num_reqs == 0;
-    }
-
-    std::map<key_t, val_t>& data() {
-        return _data;
-    }
-
-    void try_wake_up() {
-        if(_num_reqs == 0) {
-            wait_cond.notify_one();
-        }
-    }
-    // wait for all responses
-    void wait_for_rsps() {
-        std::unique_lock<std::mutex> lk(cond_mut);
-        wait_cond.wait(lk, [this] { return _num_reqs == 0;});
-    }
-
-private:
-    // node_id recevs
-    //std::map<int, bool> _rsp_rcv_flags;
-    //std::map<key_t, val_t> _data;
-    //std::mutex mut;
-
-    std::mutex cond_mut;
-    std::condition_variable wait_cond;
-    std::atomic<int> _num_reqs = 0;
-};  // end struct Parcel
-
-*/
-
-template<class Key, class Val, class GradPramProcMethod>
-class GlobalPullAccess {
+ *  key-value
+ *  key-grad
+ */
+template<class Key, class Val, class Grad, class GradPramProcMethod>
+class GlobalPullAccess : public VirtualObject {
 
 GradPramProcMethod _cache_access;
 
 public:
     typedef Key key_t;
     typedef Val val_t;
+    typedef Grad grad_t;
     typedef std::pair<key_t, val_t> pull_val_t;
 
     void pull() {
-        std::map<key_t, val_t> &vals = global_param_cache().params();
         // node_id : vals
         std::map<int, std::vector<pull_val_t> > node_reqs;
-        // split to diffent nodes
-        std::shard_ptr<RecvParcel> recv_parcel(new RecvParcel);
-        recv_parcel->data() = std::move(vals);
+        arrange_local_vals(node_reqs);
+        // send message to each nodes
+        send(node_reqs);
+    }
 
+protected:
+    void arrange_local_vals(std::map<int, std::vector<pull_val_t> > &node_reqs) {
+        auto &vals = param_cache.params();
         for( auto& item : vals) {
             auto& key = item.first;
             auto& val = item.second;
 
             int node_id = global_hashfrag().to_node_id(item.key);
+            if(node_reqs.count(node_id) == 0) {
+                node_reqs[node_id] = std::move(std::vector<pull_val_t>());
+            }
             node_reqs[node_id].push_back(item);
         }
-        // send message to each nodes
-        send(recv_parcel, node_reqs);
     }
-
-protected:
 
     void send(
         //std::shared_ptr<RecvParcel> &recv_parcel, 
@@ -124,8 +80,7 @@ protected:
     }
 
 private:
-    // determine whether the access is blocked by former push
-    //is_blocked = true;  
+    auto &param_cache = global_param_cache<key_t, val_t, grad_t>();
 };  // class GlobalPullAccess
 
 
