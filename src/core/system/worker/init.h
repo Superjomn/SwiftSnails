5374f3c8b6076aba7b47635f31fdd3a331f3acfa
Superjom <superjom@gmail.com>
2015-03-26 19:32:00 +0800

    worker.h code clean after statebarrier is added

--- src/core/system/worker/init.h
+++ src/core/system/worker/init.h
@@ -16,67 +16,10 @@ namespace swift_snails {
  *
  * register message_classes  and response_handlers
  */
-//template<typename Route>
-class NodeTransferInit;
-
-class InitWaitBarrier {
-public:
-
-    explicit InitWaitBarrier() {
-    }
-    void set_timespan(int seconds) {
-        _timer.set_time_span(seconds);
-    }
-
-    void start_timeout_thread() {
-        std::function<void()> timeout_try_unblock = [this] {
-            LOG(WARNING) << "master will wait for " << _timer.time_span() << " s";
-            std::this_thread::sleep_for( std::chrono::milliseconds(1000 + 1000 * _timer.time_span() ));
-            if(! response_arrived) {
-                LOG(ERROR) << "master wait *timeout* and didn't get a response";
-                CHECK(1 == 2) << "set ERROR";
-            }
-            std::function<void()> handle = []{};
-            barrier.unblock(handle);
-        };
-
-        std::thread t(timeout_try_unblock);
-        t.detach();
-    }
-
-    void block() {
-        std::function<void()> set_flag = [] { };
-        std::function<bool()> cond_func = [this]() {
-            return response_arrived;
-        };
-        barrier.block(set_flag, cond_func);
-    }
-
-    void try_unblock() {
-        std::function<void()> handler = []{};
-        barrier.unblock(handler);
-    }
-
-    void set_response_arrive() {
-        response_arrived = true;
-    }
-
-private:
-    bool response_arrived = false;
-    CompBarrier barrier;
-    Timer _timer;
-};
-
 class NodeTransferInit {
 public:
     explicit NodeTransferInit()
-    {
-        //global_config().register_config("master_addr", "");
-        // TODO load config 
-        //int timeout = global_config().get_config("init_timeout").to_int32();
-        //_init_barrier.set_timespan(timeout);
-        //route_init_barrier.set_unblock_state(route_init_finish_flag);
-    }
+    { }
 
     void operator() () {
         register_master();
@@ -89,14 +32,11 @@ protected:
         std::string addr = global_config().get_config("master_addr").to_string();
         LOG(WARNING) << "register master:\t" << addr;
         gtransfer.route().register_node(0, std::move(addr));
-        //LOG(WARNING) << "local register server ...";
-        //std::string addr = gtransfer.recv_addr();
-        //gtransfer.route().register_node_(true, std::move(addr));
     }
     
     // register local address to remote master server
     void register_local_node_to_master() {
-        //auto& gtransfer = global_transfer<ServerWorkerRoute>();
+
         LOG(INFO) << "local listen addr:\t" << gtransfer.recv_addr();
         Addr ip(gtransfer.recv_addr());
         LOG(INFO) << "after init ip.to_string():\t" << ip.to_string();
@@ -115,9 +55,6 @@ protected:
             // init route
             response->cont >> gtransfer.route();
             gtransfer.set_client_id(response->meta.client_id);
-
-            //_init_barrier.set_response_arrive();
-            //_init_barrier.try_unblock();    // continue running
             // timeout's unit is seconds
             route_init_barrier.set_state_valid();
             route_init_barrier.try_unblock();
@@ -137,17 +74,13 @@ protected:
             CHECK(1 == 2) << "[worker] init timeout!";
         });
         route_init_barrier.block();
-        //_init_barrier.start_timeout_thread();
-        //_init_barrier.block();
     }
 
 private:
     std::string _master_addr;
     //Barrier _init_barrier;
-    InitWaitBarrier _init_barrier;
     Transfer<ServerWorkerRoute>& gtransfer = global_transfer<ServerWorkerRoute>();
     // route init barrier
-    //bool route_init_finish_flag = false;
     StateBarrier route_init_barrier;
     
 
