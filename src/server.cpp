986a87ae02381b4539141a6fe2e2ff9593d2fc32
Superjom <superjom@gmail.com>
2015-03-27 10:38:49 +0800

    unify both worker and server's init to node_init.h

---
+++ src/server.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "utils/all.h"
+#include "core/system/node_init.h"
+#include "core/system/worker/init_config.h"
+using namespace swift_snails;
+using namespace std;
+using namespace fms;
+
+int main(int argc, char* argv[]) {
+    // init config
+    CMDLine cmdline(argc, argv);
+    string param_config_path = cmdline.registerParameter("config", "path of config file");
+    string param_help = cmdline.registerParameter("help", "this screen");
+    // parse parameters
+    if(cmdline.hasParameter(param_help) || argc == 1) {
+        cout << endl;
+        cout << "===================================================================" << endl;
+        cout << "server main program";
+        cout << "===================================================================" << endl;
+        cmdline.print_help();
+        cout << endl;
+        cout << endl;
+        return 0;
+    }
+    if(!cmdline.hasParameter(param_config_path)) {
+        LOG(ERROR) << "missing parameter: config";
+        return 0;
+    }
+    std::string config_path = cmdline.getValue(param_config_path);
+
+    worker_init_configs();
+
+    global_config().load_conf(config_path);
+    global_config().parse();
+
+    NodeTransferInit node_transfer_init;
+    NodeHashfragInit node_hashfrag_init;
+
+    node_transfer_init(true);
+    node_hashfrag_init();
+    
+    return 0;
+};
