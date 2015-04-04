071f44714e222bfc0d922b8fc829683a7e98987e
Superjom <superjom@gmail.com>
2015-04-04 17:49:28 +0800

    add buffer transformation

--- src/apps/word2vec/param.h
+++ src/apps/word2vec/param.h
@@ -17,10 +17,23 @@ struct Word2VecGrad {
         v.reset(new Vec::zero(len_vec));
     }
 
+    Word2VecGrad& operator= (const Word2VecGrad& other) {
+        *_h = other.h_grad();
+        *_v = other.v_grad();
+    }
+
     void merge_with(const Word2VecGrad &other) {
         *_h += other.h_grad();
         *_v += otehr.v_grad();
         n += other.n();
+    }
+
+    void norm() const {
+        if(_n > 0) {
+            h_grad() = h_grad() / _n;
+            v_grad() = v_grad() / _n;
+        }
+        _n = 0;
     }
 
     Word2VecGrad normed() const {
@@ -57,6 +70,10 @@ struct Word2VecGrad {
     int n() const {
         return _n;
     }
+    int len_vec() {
+        return len_vec;
+    }
+
 
 private:
     int len_vec{0};
@@ -83,12 +100,57 @@ public:
             this->_h.reset(new Vec::Zero(len_vec));
             this->_v.reset(new Vec::Zero(len_vec));
         }
+
+        grad.init(len_vec);
+    }
+    /*
+     * copy value without copy grad
+     */
+    void copy_value(const Word2VecParam& other) {
+        _h = other.h();
+        _v = other.v();
+    }
+
+    Word2VecParam& operator= (const Word2VecGrad &other) {
+        h() = other.h();
+        v() = other.v();
+        h2sum() = other.h2sum();
+        v2sum() = other.v2sum();
+    }
+
+    const Vec& h() const {
+        return *_h;
+    }
+    const Vec& v() const {
+        return *_v;
+    }
+    const Vec& h2sum() const {
+        return *_h2sum;
+    }
+    const Vec& v2sum() const {
+        return *_v2sum;
+    }
+    Vec& h() {
+        return *_h;
+    }
+    Vec& v() {
+        return *_v;
+    }
+    Vec& h2sum() {
+        return *_h2sum;
+    }
+    Vec& v2sum() {
+        return *_v2sum;
+    }
+    int len_vec() {
+        return len_vec;
     }
 
 private:
     std::unique_ptr<Vec> _h;
     std::unique_ptr<Vec> _v;
+    std::unique_ptr<Vec> _h2sum;
+    std::unique_ptr<Vec> _v2sum;
     int len_vec = len_vec;
-    Word2VecGrad grad; 
 };  // class Word2VecParam
 
