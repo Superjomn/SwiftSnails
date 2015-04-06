b35eefb9742efd17c8d112bcc09385ff8edbdd10
Superjom <superjom@gmail.com>
2015-04-06 12:02:49 +0800

    fix word2vec access_method and param

--- src/apps/word2vec/access_method.h
+++ src/apps/word2vec/access_method.h
@@ -8,11 +8,11 @@
 using namespace std;
 using namespace swift_snails;
 
-typedef size_t          key_t;
-typedef Word2VecParam   val_t;
-typedef Word2VecGrad    grad_t;
+typedef uint32_t          access_key_t;
+typedef Word2VecParam   access_val_t;
+typedef Word2VecGrad    access_grad_t;
 
-class Word2VecPullMethod : public PullAccessMethod<SparseTable<key_t, val_t>, val_t, val_t>
+class Word2VecPullMethod : public PullAccessMethod<SparseTable<access_key_t, access_val_t>, access_val_t, access_val_t>
 {
 public:
     explicit Word2VecPullMethod(int len_vec) : \
@@ -20,15 +20,12 @@ public:
     { }
 
     explicit Word2VecPullMethod() {
-    }
-    void init(int len_vec) {
-        this->len_vec = len_vec;
+        len_vec = global_config().get_config("len_vec").to_int32();
     }
 
     virtual void init_param(const key_t& key, pull_param_t &param) {
-        pull_param_t val;
-        val.init(len_vec, true);
-        param = std::move(val);
+        CHECK(len_vec > 0);
+        param.init(true);
     }
 
     virtual void get_pull_value(const key_t &key, const pull_param_t &param, pull_val_t &val) {
@@ -42,29 +39,71 @@ private:
 };  // class Word2VecPullMethod
 
 
-class Word2VecPullMethod : public PushAccessMethod<SparseTable<key_t, val_t>, grad_t, val_t> {
+class Word2VecPushMethod : public PushAccessMethod<SparseTable<access_key_t, access_val_t>, access_grad_t, access_val_t> {
+    float learning_rate = 0.01;
+public:
+    Word2VecPushMethod() {
+        learning_rate = global_config().get_config("learning_rate").to_float();
+    }
     virtual void merge_push_value(const key_t& key, push_val_t &push_val, const push_val_t &other_push_val) 
     {
         push_val.merge_with(other_push_val);
     }
-
     virtual void apply_push_value(const key_t& key, push_param_t &param, const push_val_t &grad)  
     {
-        grad.norm();
-        param.h2sum += (grad.h_grad() * grad.h_grad());
-        param.v2sum += (grad.v_grad() * grad.v_grad());
-        param.h += learning_rate * grad.h_grad / sqrt(param.h2sum);
-        param.v += learning_rate * grad.v_grad / sqrt(param.v2sum);
+        float fudge_factor = 1e-6;
+        //grad.norm();
+        CHECK(grad.n_h() == 0);
+        CHECK(grad.n_v() == 0);
+        param.h2sum() += (grad.h_grad() * grad.h_grad());
+        param.v2sum() += (grad.v_grad() * grad.v_grad());
+        param.h() += learning_rate * 
+                grad.h_grad() / (sqrt(param.h2sum() + fudge_factor ));
+
+        param.v() += learning_rate * 
+                grad.v_grad() / (sqrt(param.v2sum() + fudge_factor ));
+
+        /*
+        RAW_LOG(INFO, "key:\t%u", key);
+        RAW_LOG(INFO, "h2sum:\t%s", param.h2sum().to_str().c_str());
+        RAW_LOG(INFO, "v2sum:\t%s", param.v2sum().to_str().c_str());
+        RAW_LOG(INFO, "h_grad:\t%s", grad.h_grad().to_str().c_str());
+        RAW_LOG(INFO, "v_grad:\t%s", grad.v_grad().to_str().c_str());
+        RAW_LOG(INFO, "h:\t%s", param.h().to_str().c_str());
+        RAW_LOG(INFO, "v:\t%s", param.v().to_str().c_str());
+        */
     }
+    /*
+    virtual void apply_push_value(const key_t& key, push_param_t &param, const push_val_t &grad)  
+    {
+        //grad.norm();
+        CHECK(grad.n_h() == 0);
+        CHECK(grad.n_v() == 0);
+        //param.h2sum() += (grad.h_grad() * grad.h_grad());
+        //param.v2sum() += (grad.v_grad() * grad.v_grad());
+        //RAW_LOG(INFO, "h2sum:\t%s", param.h2sum().to_str().c_str());
+        //RAW_LOG(INFO, "v2sum:\t%s", param.v2sum().to_str().c_str());
+        param.h() += learning_rate * 
+                grad.h_grad() ;// / sqrt(param.h2sum());
+
+        param.v() += learning_rate * 
+                grad.v_grad() ; // / sqrt(param.v2sum());
+
+        RAW_LOG(INFO, "h:\t%s", param.h().to_str().c_str());
+        RAW_LOG(INFO, "v:\t%s", param.v().to_str().c_str());
+    }
+    */
 };  // class Word2VecPullMethod
 
 
-BinaryBuffer& operator<< (BinaryBuffer &bb, const Word2VecGrad& grad) {
-    bb << grad.len_vec;
+
+BinaryBuffer& operator<< (BinaryBuffer &bb, Word2VecGrad& grad) {
+    grad.norm();
+    bb << grad.len_vec();
     // input h v
     for(int i = 0; i < grad.len_vec(); i++) {
-        bb << grad.h()[i];
-        bb << grad.v()[i];
+        bb << grad.h_grad()[i];
+        bb << grad.v_grad()[i];
     }
     return bb;
 }
@@ -73,31 +112,41 @@ BinaryBuffer& operator<< (BinaryBuffer &bb, const Word2VecGrad& grad) {
 BinaryBuffer& operator>> (BinaryBuffer &bb, Word2VecGrad& grad) {
     int len_vec;
     bb >> len_vec;
-    CHECK(grad.len_vec() == 0) << "grad should be empty to write";
-    grad.init(len_vec);
+    CHECK(grad.len_vec() > 0);
     for(int i = 0; i < len_vec; i++) {
-        bb >> grad.h()[i];
-        bb >> grad.v()[i];
+        bb >> grad.h_grad()[i];
+        bb >> grad.v_grad()[i];
     }
     return bb;
 }
 
-BinaryBuffer&& operator>> (BinaryBuffer &bb, const Word2VecParam &param) {
+BinaryBuffer& operator<< (BinaryBuffer &bb, const Word2VecParam &param) {
     bb << param.len_vec();
     for(int i = 0; i < param.len_vec(); i ++) {
         bb << param.h()[i];
         bb << param.v()[i];
     }
+    return bb;
 }
 
-BinaryBuffer&& operator>> (BinaryBuffer &&bb, Word2VecParam & param) {
-    CHECK(param.len_vec() == 0) << "param should be empty to write";
+BinaryBuffer& operator>> (BinaryBuffer &bb, Word2VecParam & param) {
     int len_vec{0};
     bb >> len_vec;
-    param.init(len_vec, false);
+    param.init(false);
 
     for(int i = 0; i < len_vec; i++) {
         bb >> param.h()[i];
         bb >> param.v()[i];
     }
+    return bb;
 }
+/*
+// output parameter
+std::ostream& operator<< (std::ostream &os, const Word2VecParam& param) {
+    const auto& v = param.v();
+    for(int i = 0; i < param.len_vec(); i++) {
+        os << v[i] << " ";
+    }
+    return os;
+}
+*/
--- src/apps/word2vec/param.h
+++ src/apps/word2vec/param.h
@@ -1,57 +1,90 @@
-#include <iostream>
+#pragma once
+#include "../../utils/all.h"
+using namespace swift_snails;
 /*
  * adagrad support
  */
 struct Word2VecGrad {
-    explicit Word2VecGrad (int len_vec) :
-        len_vec(len_vec)
-    { 
-        init(len_vec);
+
+    Word2VecGrad() {
+        if(_len_vec == 0) 
+            _len_vec = global_config().get_config("len_vec").to_int32();
+        init();
     }
 
-    explicit Word2VecGrad {
+    Word2VecGrad(const Word2VecGrad& other) {
+        init();
+        for(int i = 0; i < len_vec(); i++) {
+            h_grad()[i] = other.h_grad()[i];
+            v_grad()[i] = other.v_grad()[i];
+            n_h() = other.n_h();
+            n_v() = other.n_v();
+        }
     }
-    void init(int len_vec) {
-        this->len_vec = len_vec;
-        h.reset(new Vec::zero(len_vec));
-        v.reset(new Vec::zero(len_vec));
+
+    void init() {
+        CHECK(_len_vec > 0);
+        if(!_h) {
+            //RAW_DLOG(INFO, ">  h reset new memory");
+            _h.reset(new Vec(_len_vec));
+            _v.reset(new Vec(_len_vec));
+        }
     }
 
     Word2VecGrad& operator= (const Word2VecGrad& other) {
-        *_h = other.h_grad();
-        *_v = other.v_grad();
+        init();
+        h_grad() = other.h_grad();
+        v_grad() = other.v_grad();
+        n_h() = other.n_h();
+        n_v() = other.n_v();
     }
 
     void merge_with(const Word2VecGrad &other) {
         *_h += other.h_grad();
-        *_v += otehr.v_grad();
-        n += other.n();
+        *_v += other.v_grad();
+        n_v() += other.n_v();
+        n_h() += other.n_h();
     }
 
-    void norm() const {
-        if(_n > 0) {
-            h_grad() = h_grad() / _n;
-            v_grad() = v_grad() / _n;
+    void accu_v(Vec& grad) {
+        v_grad() += grad;
+        _n_v ++;
+    }
+
+    void accu_h(Vec& grad) {
+        h_grad() += grad;
+        _n_h ++;
+    }
+
+    void norm() {
+        if(_n_h > 0) {
+            h_grad() /=  static_cast<double>(_n_h);
         }
-        _n = 0;
+        if(_n_v > 0) {
+            v_grad() /= static_cast<double>(_n_v);
+        }
+        _n_h = 0;
+        _n_v = 0;
     }
-
+    /*
     Word2VecGrad normed() const {
-        Word2VecGrad val(len_vec);
-        if(_n > 0) {
-            val.h_grad() = h_grad() / _n;
-            val.v_grad() = v_grad() / _n;
+        Word2VecGrad val(_len_vec);
+        if(_n_h > 0) {
+            val.h_grad() /= _n_h;
+        }
+        if(_n_v > 0) {
+            val.v_grad() /= _n_v;
         }
         return std::move(val);
     }
+    */
 
     void reset() {
-        _n = 0.0;
-        for(auto& v : h_grad()) {
-            v = 0.0;
-        }
-        for(auto& v : v_grad()) {
-            v = 0.0;
+        _n_v = 0;
+        _n_h = 0;
+        for(int i = 0; i < len_vec(); i++) {
+            v_grad()[i] = 0.0;
+            h_grad()[i] = 0.0;
         }
     }
 
@@ -67,20 +100,44 @@ struct Word2VecGrad {
     Vec& v_grad() {
         return *_v;
     }
-    int n() const {
-        return _n;
+    int& n_v() {
+        return _n_v;
     }
-    int len_vec() {
-        return len_vec;
+    int& n_h() {
+        return _n_h;
+    }
+    int n_h() const {
+        return _n_h;
+    }
+    int n_v() const {
+        return _n_v;
+    }
+    int len_vec() const {
+        return _len_vec;
+    }
+    friend std::ostream& operator<< (std::ostream& os, const Word2VecGrad& grad) {
+        os << "nh\t" << grad.n_h() << "\tnv\t" << grad.n_v() << "\t";
+        os << "h:\t";
+        for(int i = 0; i < grad.len_vec(); i++) {
+            os << grad.h_grad()[i] << " ";
+        }
+        os << "\t";
+        os << "v:\t";
+        for(int i = 0; i < grad.len_vec(); i++) {
+            os << grad.v_grad()[i] << " ";
+        }
+        return os;
     }
 
 
 private:
-    int len_vec{0};
-    int _n{0};
+    static int _len_vec;
+    int _n_v{0};
+    int _n_h{0};
     std::unique_ptr<Vec> _h;
     std::unique_ptr<Vec> _v;
 };  // class Word2VecGrad
+
 
 
 /*
@@ -88,34 +145,52 @@ private:
  */
 struct Word2VecParam {
 public:
-    explicit Word2VecParam() {
+    Word2VecParam() {
+        if(_len_vec == 0) 
+            _len_vec = global_config().get_config("len_vec").to_int32();
+        init();
     }
 
-    void init(int len_vec, bool rand_init = false) {
-        this->len_vec = len_vec;
-        if(rand_init) {
-            this->_h.reset(new Vec::random(len_vec));
-            this->_v.reset(new Vec::random(len_vec));
-        } else {
-            this->_h.reset(new Vec::Zero(len_vec));
-            this->_v.reset(new Vec::Zero(len_vec));
-        }
+    Word2VecParam(const Word2VecParam& other) {
+        init();
+        h() = other.h();
+        v() = other.v();
+        h2sum() = other.h2sum();
+        v2sum() = other.v2sum();
+    }
 
-        grad.init(len_vec);
+    void init(bool rand_init = false) {
+        CHECK(_len_vec > 0);
+        if(!_h) {
+            _h.reset(new Vec(_len_vec));
+            _v.reset(new Vec(_len_vec));
+            _h2sum.reset(new Vec(_len_vec));
+            _v2sum.reset(new Vec(_len_vec));
+        }
+        if(rand_init) {
+            std::random_device rd;
+            std::normal_distribution<> d(0,1);
+            for(int i = 0; i < _len_vec; i++) {
+                h()[i] = d(rd);
+                v()[i] = d(rd);
+            }
+        } 
     }
     /*
      * copy value without copy grad
      */
     void copy_value(const Word2VecParam& other) {
-        _h = other.h();
-        _v = other.v();
+        h() = other.h();
+        v() = other.v();
     }
 
-    Word2VecParam& operator= (const Word2VecGrad &other) {
+    Word2VecParam& operator= (const Word2VecParam &other) {
+        init();
         h() = other.h();
         v() = other.v();
         h2sum() = other.h2sum();
         v2sum() = other.v2sum();
+        return *this;
     }
 
     const Vec& h() const {
@@ -142,8 +217,20 @@ public:
     Vec& v2sum() {
         return *_v2sum;
     }
-    int len_vec() {
-        return len_vec;
+    int len_vec() const {
+        return _len_vec;
+    }
+
+    friend std::ostream& operator<< (std::ostream &os, const Word2VecParam &param) {
+        os << "h:\t";
+        for(int i = 0; i < param.len_vec(); i ++) {
+            os << param.h()[i] << " ";
+        }
+        os << "v:\t";
+        for(int i = 0; i < param.len_vec(); i ++) {
+            os << param.v()[i] << " ";
+        }
+        return os;
     }
 
 private:
@@ -151,6 +238,10 @@ private:
     std::unique_ptr<Vec> _v;
     std::unique_ptr<Vec> _h2sum;
     std::unique_ptr<Vec> _v2sum;
-    int len_vec = len_vec;
+    static int _len_vec;
 };  // class Word2VecParam
+
+
+int Word2VecGrad::_len_vec = 0;
+int Word2VecParam::_len_vec = 0;
 
