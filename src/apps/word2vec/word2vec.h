4be3c5caf6cee669ade9d3b214f1a284a54a6b8f
Superjom <superjom@gmail.com>
2015-04-12 19:37:01 +0800

    code clean for word2vec.h

--- src/apps/word2vec/word2vec.h
+++ src/apps/word2vec/word2vec.h
@@ -25,11 +25,9 @@ public:
         window(window), 
         negative(negative), 
         len_vec(len_vec),
-        param_cache(global_param_cache<key_t, val_t, grad_t>()),
         pull_access(global_pull_access<key_t, val_t, grad_t>()),
         push_access(global_push_access<key_t, val_t, grad_t>())
     { 
-        //_num_iters = global_config().get_config("num_iters").to_int32();
         learning_rate = global_config().get_config("learning_rate").to_float();
         sample = global_config().get_config("sample").to_float();
         minibatch = global_config().get_config("minibatch").to_int32();
@@ -42,7 +40,6 @@ public:
         for(int i = 0; i < _num_iters; i ++) {
             LOG(WARNING) << i << " th iteration";
             train_iter(_async_channel_thread_num);
-            //param_cache.inc_num_iters();
         }
     }
 
@@ -78,18 +75,21 @@ private:
 
         double global_error = 0;
         size_t error_counter = 0;
+        size_t bad_error_counter = 0;
 
-        auto trainer = [this, &global_error, &error_counter] (std::vector<rcd_t::feas_t> &batch, std::unordered_set<key_t> &local_keys) {
+        auto trainer = [this, &global_error, &error_counter, &bad_error_counter] 
+        (std::vector<rcd_t::feas_t> &batch, param_cache_t &param_cache, std::unordered_set<key_t> &local_keys) 
+        {
             double error;
-            pull_access.pull_with_barrier(local_keys);
+            pull_access.pull_with_barrier(local_keys, param_cache);
             for(auto& feas : batch) {
-                error = learn_record(std::move(feas));
+                error = learn_record(std::move(feas), param_cache);
                 global_error += error;
                 error_counter ++;
             }
             // finish mini-batch training 
             // and PUSH the grads
-            push_access.push_with_barrier(local_keys);
+            push_access.push_with_barrier(local_keys, param_cache);
             local_keys.clear();
             batch.clear();
         };
@@ -97,6 +97,8 @@ private:
         auto minibatch_task = [this, &queue, &global_error, &error_counter, &trainer] {
             std::vector<rcd_t::feas_t> _minibatch;
             std::unordered_set<key_t> local_keys;
+            param_cache_t param_cache;  // local param
+            
             for(;;) {
                 // get one line from queue
                 std::string line;
@@ -112,13 +114,13 @@ private:
                     _minibatch.push_back(std::move(rcd.feas));
                     if(_minibatch.size() == minibatch) {
                         // pull parameters
-                        trainer(_minibatch, local_keys);
+                        trainer(_minibatch, param_cache, local_keys);
                     }
                 }
             } // end for
 
             if(! _minibatch.empty()) {
-                trainer(_minibatch, local_keys);
+                trainer(_minibatch, param_cache, local_keys);
             }
         };
 
@@ -140,34 +142,10 @@ private:
         t.join();
         std::fclose(file);
 
-        RAW_LOG(INFO, ">  train error:\t%f", global_error/error_counter);
+        RAW_LOG(INFO, ">  train error:\t%f\tbad_error:\t", global_error/error_counter, bad_error_counter);
     }
 
-/*
-    void learn_record(rcd_t::feas_t && wids) {
-        std::mt19937 rng(rd());
-        std::uniform_int_distribution<int> rand(0, window);
-        Vec labels(negative + 1);
-        labels[0] = 1.0;
-
-
-        for(int pos = 0; pos < wids.size(); pos ++) {
-            int reduced_window = rand(rng);
-            int start = std::max(0, pos - window + reduced_window);
-            key_t& word = wids[pos].first;
-
-            for(size_t pos2 = start; pos2 < pos + window + 1 - reduced_window; pos2 ++){
-                key_t& word2 = wids[pos2].first;
-
-                if( ! pos2 == pos) {
-                    train_sg_pair(word, word2, labels);
-                }
-            }
-        }
-    }
-*/
-
-    double learn_record(rcd_t::feas_t && sen) {
+    double learn_record(rcd_t::feas_t && sen, param_cache_t &param_cache) {
         long long a, b, d, word, last_word;
         size_t sentence_length = sen.size(), 
                 sentence_position = 0;
@@ -175,8 +153,7 @@ private:
         long long l1, l2, c, target, label;
         double f, g;
         clock_t now;
-        //std::unique_ptr<double> neu1 = new double[len_vec()];
-        //std::unique_ptr<double> neu1e = new double[len_vec()];
+
         std::mt19937 rng(rd());
         std::uniform_int_distribution<int> int_rand(0, table_size);
         std::default_random_engine float_gen;
@@ -229,8 +206,12 @@ private:
                     g = (label - f);
                     // calculate error
                     double error = label == 0 ? - log(1-f) : -log(f);
-                    global_error += error;
-                    error_counter ++;
+                    if(error > 0.00000001 && error < 9000) {
+                        global_error += error;
+                        error_counter ++;
+                    } else {
+                        //bad_error_counter ++;
+                    }
                     neu1e += (g * v2); // * learning_rate
 
                     Vec grad_v2 = std::move(g * v1);
@@ -249,69 +230,6 @@ private:
         return error_counter == 0 ? 0 : global_error / error_counter;
     }
 
-
-    void train_sg_pair(key_t word, key_t word2, const Vec& labels, bool train_w1=true, bool train_w2=true) {
-        CHECK_GT(param_cache.local_keys().size(), 0) << "local_keys should be inited";
-        Vec l1 = param_cache.params()[word2].v();
-
-        Vec neu1e = Vec(len_vec);
-        // generate noise         
-        std::mt19937 rng(rd());
-        std::uniform_int_distribution<int> rand(0, word_freg.size());
-
-        std::vector<key_t> word_indices;
-        word_indices.push_back(word);
-        while(word_indices.size() < negative + 1) {
-            int id = rand(rng);
-            key_t w = table2word_freq[id].first;
-            if (w != word) word_indices.push_back(w);
-        }
-        // prepare l2b
-        // negative hidden vector of the selected words
-        std::vector<Vec> l2b;
-        for(size_t i = 0; i < word_indices.size(); ++i ) {
-            key_t id = word_indices[i];
-            Vec h = param_cache.params()[id].h();
-            l2b.push_back(std::move(h));
-        }
-        // prepare fb
-        Vec fb(word_indices.size());
-        for(size_t i = 0 ; i < word_indices.size(); i++) {
-            CHECK_GT(l2b[i].size(), 0);
-            fb[i] = 1.0 / (1.0 + 1.0 / exp(l1.dot(l2b[i])));
-        }
-        Vec gb = (labels - fb);
-
-        auto Outer = outer(gb, l1);
-
-        if(train_w1) {
-            for(size_t i = 0; i < word_indices.size(); i++) {
-                const auto wid = word_indices[i];
-                // update word vector
-                auto &_h = param_cache.params()[wid].h();
-                CHECK_EQ(_h.size(), Outer[i].size()); 
-
-                _h += Outer[i] * learning_rate;
-                param_cache.grads()[wid].accu_h(Outer[i]);
-            }
-        }
-
-        Vec gb_dot_l2b(len_vec);
-        for(size_t i = 0; i < len_vec; i++) {
-            Vec l2b_T(word_indices.size());
-            for(size_t j = 0; j < word_indices.size(); j++) {
-                l2b_T[j] = l2b[j][i]; 
-            }
-            gb_dot_l2b[i] = gb.dot(l2b_T);
-        }
-        neu1e += gb_dot_l2b;
-
-        if(train_w2) {
-            // update v
-            param_cache.params()[word2].v() += neu1e * learning_rate;
-            param_cache.grads()[word2].accu_v(neu1e);
-        }
-    }
 
     void get_word_freq(int thread_num) {
         LOG(WARNING) << "... get_word_freq";
@@ -375,7 +293,7 @@ private:
     int len_vec{0};
     int _num_iters{0};
     std::random_device rd;
-    param_cache_t &param_cache;
+    //param_cache_t &param_cache;
     pull_access_t &pull_access;
     push_access_t &push_access;
     //std::vector<key_t> local_keys;
